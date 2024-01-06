97e8840a8e8d17cd0e945eb7a9d2bd2677e78f6b
Tastan Kulmeshkenov <tastankulmeshkenov@gmail.com>
2024-01-05 19:22:17 -0600

    updated db

--- db.js
+++ db.js
@@ -9,20 +9,20 @@ require("dotenv").config(); // testing client
 
 // use getDatabaseUri() when testing
 // because render.com did not work with a db connection string
-db = new Client({
-    connectionString: getDatabaseUri()
-})
-db.connect();
-
 // db = new Client({
-//     user: process.env.USERNAME,
-//     host: process.env.HOST,
-//     database: process.env.DB_NAME,
-//     password: process.env.PASSWORD,
-//     port: process.env.DB_PORT,
+//     connectionString: getDatabaseUri()
 // })
-
 // db.connect();
+
+db = new Client({
+    user: process.env.USERNAME,
+    host: process.env.HOST,
+    database: process.env.DB_NAME,
+    password: process.env.PASSWORD,
+    port: process.env.DB_PORT,
+})
+
+db.connect();
 
 
 module.exports = db;
