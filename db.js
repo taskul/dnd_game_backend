a841731499e388b2cbba952690f3d81e24b3faab
Tastan Kulmeshkenov <tastankulmeshkenov@gmail.com>
2024-01-06 11:50:12 -0600

    updated db

--- db.js
+++ db.js
@@ -26,7 +26,5 @@ db = new Client({
 })
 
 db.connect()
-    .then(() => console.log('Connected to the database'))
-    .catch((err) => console.error('Error connecting to the database', err));
 
 module.exports = db;
