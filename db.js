98957f0fccaaeae23512985cecb6d3b1e1e23288
Tastan Kulmeshkenov <tastankulmeshkenov@gmail.com>
2024-01-26 21:29:31 -0600

    updated db

--- db.js
+++ db.js
@@ -3,7 +3,7 @@
 
 const { Client } = require("pg");
 // const { getDatabaseUri } = require("./config");
-
+const { Pool } = require('pg');
 let db;
 require("dotenv").config(); // testing client
 
@@ -14,16 +14,23 @@ require("dotenv").config(); // testing client
 // })
 // db.connect();
 
-db = new Client({
-    user: process.env.DB_USERNAME,
-    host: process.env.HOST,
-    database: process.env.DB_NAME,
-    password: process.env.PASSWORD,
-    port: process.env.DB_PORT,
+// db = new Client({
+//     user: process.env.DB_USERNAME,
+//     host: process.env.HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.PASSWORD,
+//     port: process.env.DB_PORT,
+//     ssl: {
+//         sslmode: 'require',
+//     },
+// })
+
+db = new Pool({
+    connectionString: process.env.DATABASE_URL,
     ssl: {
-        sslmode: 'require',
+      require: true,
     },
-})
+  });
 
 db.connect()
 
