29774915c2839ee319abf750b498f48d2e089f7e
Tastan Kulmeshkenov <tastankulmeshkenov@gmail.com>
2024-01-26 22:18:40 -0600

    updated db

--- db.js
+++ db.js
@@ -1,31 +1,11 @@
 "use strict";
 /** Database setup for dnd_game. */
 
-const { Client } = require("pg");
-// const { getDatabaseUri } = require("./config");
 const { Pool } = require('pg');
-let db;
-require("dotenv").config(); // testing client
+require("dotenv").config(); 
 
-// use getDatabaseUri() when testing
-// because render.com did not work with a db connection string
-// db = new Client({
-//     connectionString: getDatabaseUri()
-// })
-// db.connect();
 
-// db = new Client({
-//     user: process.env.DB_USERNAME,
-//     host: process.env.HOST,
-//     database: process.env.DB_NAME,
-//     password: process.env.PASSWORD,
-//     port: process.env.DB_PORT,
-//     ssl: {
-//         sslmode: 'require',
-//     },
-// })
-
-db = new Pool({
+let db = new Pool({
     user: process.env.DB_USERNAME,
     host: process.env.HOST,
     database: process.env.DB_NAME,
@@ -35,13 +15,6 @@ db = new Pool({
         sslmode: 'require',
     },
 })
-
-// db = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//       require: true,
-//     },
-//   });
 
 db.connect()
 
