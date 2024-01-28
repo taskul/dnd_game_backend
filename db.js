76259c80b4cea9b9ee3be7585262810c4efa54a8
Tastan Kulmeshkenov <tastankulmeshkenov@gmail.com>
2024-01-28 09:19:55 -0600

    update db

--- db.js
+++ db.js
@@ -1,19 +1,18 @@
 "use strict";
 /** Database setup for dnd_game. */
 
+const { Client } = require("pg");
+// const { getDatabaseUri } = require("./config");
 const { Pool } = require('pg');
-require("dotenv").config(); 
+let db;
+require("dotenv").config(); // testing client
 
-
-let db = new Pool({
+db = new Client({
     user: process.env.DB_USERNAME,
     host: process.env.HOST,
     database: process.env.DB_NAME,
     password: process.env.PASSWORD,
     port: process.env.DB_PORT,
-    ssl: {
-        sslmode: 'require',
-    },
 })
 
 db.connect()
